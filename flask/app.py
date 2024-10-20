b2995ab7156317962fd8b90f308cf1866b4ac193
unsigned <abc@gmail.com.com>
2024-10-20 05:49:23 -0700

    app cursor

--- flask/app.py
+++ flask/app.py
@@ -6,16 +6,16 @@ import mysql.connector
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user_info.db"
-db = SQLAlchemy(app)
-conn = mysql.connector.connect(
-    host="sql.freedb.tech",
-    user="freedb_user_101",
-    password="7aGdxGx*qP&Ymd7",
-    database="freedb_127.0.0.1",
-)
-date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc))
-cursor = conn.cursor()
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user_info.db"
+# db = SQLAlchemy(app)
+# conn = mysql.connector.connect(
+#     host="sql.freedb.tech",
+#     user="freedb_user_101",
+#     password="7aGdxGx*qP&Ymd7",
+#     database="freedb_127.0.0.1",
+# )
+# date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+# cursor = conn.cursor()
 
 
 @app.route("/home")
@@ -37,12 +37,12 @@ def register():
 def login_validation():
     email = request.form.get("email")
     password = request.form.get("password")
-    cursor.execute(
-        """SELECT * FROM `users` WHERE `email` = %s AND `password` = %s""",
-        (email, password),
-    )
-    users = cursor.fetchall()
-    return users
+    # cursor.execute(
+    #     """SELECT * FROM `users` WHERE `email` = %s AND `password` = %s""",
+    #     (email, password),
+    # )
+    # users = cursor.fetchall()
+    # return users
 
 
 if __name__ == "__main__":
